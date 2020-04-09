5b03d49b6727288f109ae3529c6cdddc12558d6d
Petar <petarst97@gmail.com>
2020-04-10 01:09:13 +0300

    add poll command

---
+++ cogs/poll.py
@@ -0,0 +1,111 @@
+import discord
+import asyncio
+from discord.ext import commands
+import random
+
+
+
+class Poll(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.emojiLetters = [
+            "\N{REGIONAL INDICATOR SYMBOL LETTER A}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER B}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER C}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER D}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER E}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER F}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER G}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER H}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER I}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER J}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER K}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER L}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER M}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER N}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER O}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER P}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER Q}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER R}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER S}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER T}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER U}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER V}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER W}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER X}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER Y}",
+            "\N{REGIONAL INDICATOR SYMBOL LETTER Z}"
+        ]
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.author.bot:
+            if message.content.startswith("+poll") or message.content.startswith("poll:") or message.content.startswith("Poll:") or message.content.startswith("+poll:") or message.content.startswith("+Poll:"):
+                messageContent = message.clean_content
+                if messageContent.find("{") == -1:
+                    await message.add_reaction('👍')
+                    await message.add_reaction('👎')
+                    await message.add_reaction('🤷')
+                else:
+                    first = messageContent.find("{") + 1
+                    second = messageContent.find("}")
+                    title = messageContent[first:second]
+
+                    # gets the # of options and assigns them to an array
+                    newMessage = messageContent[second:]
+                    loopTime = 0
+
+                    option = []
+                    for options in messageContent:
+                        # get from } [option 1]
+                        stillOptions = newMessage.find("[")
+                        if stillOptions != -1:
+                            if loopTime == 0:
+                                first = newMessage.find("[") + 1
+                                second = newMessage.find("]")
+                                second1 = second + 1
+                                option.append(newMessage[first:second])
+                                loopTime += 1
+                            else:
+                                newMessage = newMessage[second1:]
+                                first = newMessage.find("[") + 1
+                                second = newMessage.find("]")
+                                second1 = second + 1
+                                option.append(newMessage[first:second])
+                                loopTime += 1
+
+                    try:
+                        pollMessage = ""
+                        i = 0
+                        for choice in option:
+                            if not option[i] == "":
+                                if len(option) > 21:
+                                    await message.channel.send("Maximum of 20 options")
+                                    return
+                                elif not i == len(option) - 1:
+                                    pollMessage = pollMessage + "\n\n" + self.emojiLetters[i] + " " + choice
+                            i += 1
+
+                        ads = ["\n\n[Don't let eye strain ruin your day. Protect your eyes from harmful blue light using Bakery Gaming's glasses. Use code 'Poll Bot' for 20% off your order.](https://bakerygaming.store/collections/all)"]
+
+
+
+                        e = discord.Embed(title="**" + title + "**",
+                                description=pollMessage + ads[0],
+                                          colour=0x83bae3)
+                        pollMessage = await message.channel.send(embed=e)
+                        i = 0
+                        final_options = []  # There is a better way to do this for sure, but it also works that way
+                        for choice in option:
+                            if not i == len(option) - 1 and not option[i] == "":
+                                final_options.append(choice)
+                                await pollMessage.add_reaction(self.emojiLetters[i])
+                            i += 1
+                    except KeyError:
+                        return "Please make sure you are using the format 'poll: {title} [Option1] [Option2] [Option 3]'"
+            else:
+                return
+
+def setup(bot):
+    bot.add_cog(Poll(bot))
