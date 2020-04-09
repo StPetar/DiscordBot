ceb9ce4fcc43c07509755043a05c0d632f23170f
Petar <petarst97@gmail.com>
2020-04-10 02:43:15 +0300

    fix poll command remove tally command (shit too advanced for me)

--- cogs/poll.py
+++ cogs/poll.py
@@ -35,37 +35,9 @@ class Poll(commands.Cog):
         for reaction in reactions[:len(options)]:
             await react_message.add_reaction(reaction)
         embed.set_footer(text='Poll ID: {}'.format(react_message.id))
-        await ctx.edit_message(react_message, embed=embed)
-
-    @commands.command(pass_context=True)
-    async def tally(self, ctx, id):
-        poll_message = await self.bot.get_message(ctx.message.channel, id)
-        if not poll_message.embeds:
-            return
-        embed = poll_message.embeds[0]
-        if poll_message.author != ctx.message.server.me:
-            return
-        if not embed['footer']['text'].startswith('Poll ID:'):
-            return
-        unformatted_options = [x.strip() for x in embed['description'].split('\n')]
-        opt_dict = {x[:2]: x[3:] for x in unformatted_options} if unformatted_options[0][0] == '1' \
-            else {x[:1]: x[2:] for x in unformatted_options}
-        # check if we're using numbers for the poll, or x/checkmark, parse accordingly
-        voters = [ctx.message.server.me.id]  # add the bot's ID to the list of voters to exclude it's votes
-
-        tally = {x: 0 for x in opt_dict.keys()}
-        for reaction in poll_message.reactions:
-            if reaction.emoji in opt_dict.keys():
-                reactors = await self.bot.get_reaction_users(reaction)
-                for reactor in reactors:
-                    if reactor.id not in voters:
-                        tally[reaction.emoji] += 1
-                        voters.append(reactor.id)
-
-        output = 'Results of the poll for "{}":\n'.format(embed['title']) + \
-                 '\n'.join(['{}: {}'.format(opt_dict[key], tally[key]) for key in tally.keys()])
-        await ctx.send(output)
-
+        await react_message.edit(
+            embed=embed,
+        )
 
 def setup(bot):
     bot.add_cog(Poll(bot))  # Adds the Basic commands to the bot
