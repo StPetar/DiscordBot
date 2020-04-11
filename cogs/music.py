73d557d30e0efb8b18f439965c99d94ca1aec8f1
Petar <petarst97@gmail.com>
2020-04-11 16:29:00 +0300

    fix shuffle issue

--- cogs/music.py
+++ cogs/music.py
@@ -407,7 +407,8 @@ class Music(commands.Cog):
 
     @commands.command(
         name='skip',
-        description="Vote to skip a song. The requester can automatically skip.\n 3 skip votes are needed for the song to be skipped.",
+        description="Vote to skip a song. The requester can automatically skip.\n \
+         3 skip votes are needed for the song to be skipped.",
         usage='<command prefix>skip'
     )
     async def _skip(self, ctx):
@@ -474,7 +475,7 @@ class Music(commands.Cog):
             raise MusicError('Nothing in the queue.')
 
         ctx.state.songs.shuffle()
-        await ctx.message.add_reaction('✅')
+        await ctx.send('The queue has been shuffled')
 
     @commands.command(
         name='remove',
