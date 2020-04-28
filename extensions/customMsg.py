192cd8bb2821a811835d8f2a3fa775e8f58f2ad7
Petar Stoyanov <57144484+StPetar@users.noreply.github.com>
2020-04-28 12:32:09 +0300

    Clean up code and comments

--- extensions/customMsg.py
+++ extensions/customMsg.py
@@ -2,9 +2,9 @@ import sqlite3
 import discord
 from discord.ext import commands
 
-
+# This cog is used to select the channel for the custom join and leave messages
 class MessageSelect(commands.Cog):
-
+    
     def __init__(self, bot):
         self.bot = bot
 
@@ -16,17 +16,23 @@ class MessageSelect(commands.Cog):
         usage='<command prefix>bot_channel #{channel}'
     )
     async def channel_select(self, ctx, channel: discord.TextChannel):
+        # Check a level of administrator permissions, in my case this is just to manage_messages
+        # But there are a handful stricter permissions this can be changed to depending on your needs
         if ctx.message.author.guild_permissions.manage_messages:
             db = sqlite3.connect('main.sqlite')
             cursor = db.cursor()
             cursor.execute(f'SELECT channel_id FROM main WHERE guild_id = {ctx.guild.id}')
             result = cursor.fetchone()
+            
+            # If the channel is not already set, set it here
             if result is None:
                 sql = 'INSERT INTO main(guild_id, channel_id) VALUES(?, ?)'
                 val = (ctx.guild.id, channel.id)
                 cursor.execute(sql, val)
                 db.commit()
                 await ctx.send(f"Channel has been set to {channel.mention}")
+                
+            # If the channel had been set, update it to the new one
             elif result is not None:
                 sql = 'UPDATE main SET channel_id = ? WHERE guild_id = ?'
                 val = (channel.id, ctx.guild.id)
@@ -37,6 +43,9 @@ class MessageSelect(commands.Cog):
             db.close()
         return
 
+    # With this command you can have a custom normal message set
+    # I however opted to later on remake this into an embedded message
+    # So this command is currently unused in my code, check bot.py
     @commands.command(
         name='set_message',
         description='Set custom join message [currently disabled]',
@@ -44,17 +53,21 @@ class MessageSelect(commands.Cog):
         usage='<command prefix>set_message [text]'
     )
     async def msg(self, ctx, *, text):
+        # Similar to the get_channel command
+        # Check for permissions of the user trying to use the command
         if ctx.message.author.guild_permissions.manage_messages:
             db = sqlite3.connect('main.sqlite')
             cursor = db.cursor()
             cursor.execute(f'SELECT msg FROM main WHERE guild_id = {ctx.guild.id}')
             result = cursor.fetchone()
+            # Create an entry into the DB if it didn't exist
             if result is None:
                 sql = 'INSERT INTO main(guild_id, channel_id) VALUES(?, ?)'
                 val = (ctx.guild.id, text)
                 cursor.execute(sql, val)
                 db.commit()
                 await ctx.send(f"Channel has been set to '{text}'")
+            # If it did exist, update the entry
             elif result is not None:
                 sql = 'UPDATE main SET channel_id = ? WHERE guild_id = ?'
                 val = (text, ctx.guild.id)
